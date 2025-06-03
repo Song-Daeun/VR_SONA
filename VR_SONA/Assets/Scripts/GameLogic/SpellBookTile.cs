87ef587f0d169d5fa68485ba5023cff879fd4075
Song-Daeun <sde11280@hanyang.ac.kr>
2025-06-03 19:43:19 +0900

    [Feat] 마법서 칸 진입 시 이벤트 선택 canvas 띄움

--- VR_SONA/Assets/Scripts/GameLogic/SpellBookTile.cs
+++ VR_SONA/Assets/Scripts/GameLogic/SpellBookTile.cs
@@ -1,23 +1,47 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpellBookTile : MonoBehaviour
 {
-    public float additionalTime = 30f;  // 증가시킬 시간 (초)
-    private bool isUsed = false;        // 한 번만 작동하도록 방지
+    public GameObject buttonCanvas;
+    public GameObject TimeButton;
+    public GameObject MoveButton;
+    public GameObject MessageText;
+
+    public float additionalTime = 30f;
+    private bool isTimeAdded = false;
+
+    void Start()
+    {
+        buttonCanvas.SetActive(false);
+        if (TimeButton != null) TimeButton.SetActive(false);
+        if (MoveButton != null) MoveButton.SetActive(false);
+        if (MessageText != null) MessageText.SetActive(false);
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (isUsed) return;
-
         if (other.CompareTag("MainCamera"))
         {
-            slider1 timer = FindObjectOfType<slider1>();
-            if (timer != null)
-            {
-                timer.AddTime(additionalTime);
-                isUsed = true; // 중복 작동 방지
-                Debug.Log("Add 30sec: ");
-            }
+            Debug.Log("Entered Tile: " + other.name);
+            buttonCanvas.SetActive(true);
+
+            if (TimeButton != null) TimeButton.SetActive(true);
+            if (MoveButton != null) MoveButton.SetActive(true);
+            if (MessageText != null) MessageText.SetActive(true);
+        }
+    }
+
+    public void OnClickAddTime()
+    {   
+        if (isTimeAdded) return;
+
+        slider1 timer = FindObjectOfType<slider1>();
+        if (timer != null)
+        {
+            timer.AddTime(additionalTime);
+            isTimeAdded = true;
+            Debug.Log("Add 30sec from TimeButton");
         }
     }
 }
